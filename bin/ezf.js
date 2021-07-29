00fbc52a902b85610f46d54220a11686e270187d
Ryohei Kawashima <ryouhei-kawashima@techconnect.co.jp>
2021-07-29 18:22:45 +0900

    add max speed option, and speed bug fix

--- bin/ezf.js
+++ bin/ezf.js
@@ -8,6 +8,23 @@ function smc(options) {
     return execSync('sudo /Applications/smcFanControl.app/Contents/Resources/smc ' + options);
 }
 
+function getFanCount() {
+    return smc('-f').toString().split('\n')[0].slice(-1);
+}
+
+function changeAllFansForceMode(fanCount) {
+    const target = ((2 ** fanCount) - 1).toString(16);
+    smc('-k "FS! " -w 000' + target);
+}
+
+function changeFanRotationSpeed(fanNumber, speed) {
+    smc('-k F' + fanNumber + 'Tg -w ' + fanSpeedMapper(speed));
+}
+
+function fanSpeedMapper(speed) {
+    return (speed << 2).toString(16);
+}
+
 // show help
 if (argv.help || argv.h) {
     console.log("" +
@@ -15,6 +32,7 @@ if (argv.help || argv.h) {
         "  -h, --help             display help for command\n" +
         "  -s, --status           Check mac fan status\n" +
         "  -a, --auto             Change mac fan all auto mode\n" +
+        "  -m, --max              Maximize all fans speed\n" +
         "  -r, --rotate [speed]   Change mac fan rotate"
     );
     return;
@@ -34,18 +52,36 @@ if (argv.auto !== undefined || argv.a !== undefined) {
     return;
 }
 
+// maximize all fans speed
+if (argv.max !== undefined || argv.m !== undefined) {
+    // change mac fan mode
+    const fanCount = getFanCount();
+    changeAllFansForceMode(fanCount);
+
+    // [fan#0 speed, fan#1 speed...]
+    const maximumSpeeds = smc('-f')
+        .toString()
+        .split('\n')
+        .filter(x => x.indexOf('Maximum') !== -1)
+        .map(x => x.split(':')[1]
+        .slice(1));
+
+    // change mac fan rotate speed
+    for (let i = 0; i < fanCount; i++) changeFanRotationSpeed(i, maximumSpeeds[i]);
+
+    console.log("Maximize all fans speed.");
+    return;
+}
+
 // change mac fan rotate speed
 if (argv.rotate !== undefined || argv.r !== undefined) {
     // change mac fan mode
-    const fanCount = smc('-f').toString().split('\n')[0].slice(-1);
-    const target = ((2 ** fanCount) - 1).toString(16);
-    smc('-k "FS! " -w 000' + target);
+    const fanCount = getFanCount();
+    changeAllFansForceMode(fanCount);
 
     // change mac fan rotate speed
     const speed = argv.r || argv.rotate;
-    for (let i = 0; i < fanCount; i++) {
-        smc('-k F' + i + 'Tg -w ' + speed);
-    }
+    for (let i = 0; i < fanCount; i++) changeFanRotationSpeed(i, speed);
 
     console.log("changed mac fan rotation speed.");
     return;
